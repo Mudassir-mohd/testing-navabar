5be5a04ad1d6e420a8c939e130c47ee7a92178f1
mudassir mohd <mujjumohd465@gmail.com>
2025-03-06 13:59:31 +0530

    s

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openSidebar() {
+    document.getElementById('sidebar').style.left = '0';
+}
+
+function closeSidebar() {
+    document.getElementById('sidebar').style.left = '-250px';
+}
